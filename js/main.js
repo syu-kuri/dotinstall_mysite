8a2a3a5be8eac628564ca61655c070acfe5d3a44
syukuri <souki.shinohara@outlook.jp>
2021-07-23 19:55:56 +0900

    slide,contactmenuの作成

--- js/main.js
+++ js/main.js
@@ -29,4 +29,20 @@
   const nowDate = document.getElementById('nowDate');
   nowDate.insertAdjacentHTML('afterend', NowDate());
 
+  // 目次表示切り替え
+  const contentsOpen = document.getElementById('contents-open');
+  const contentsClose = document.getElementById('contents-close');
+  const contentsUl = document.getElementById('contents-ul');
+
+  contentsOpen.addEventListener('click', () => {
+    contentsOpen.classList.remove('active');
+    contentsClose.classList.add('active');
+    contentsUl.classList.remove('active');
+  });
+
+  contentsClose.addEventListener('click', () => {
+    contentsOpen.classList.add('active');
+    contentsClose.classList.remove('active');
+    contentsUl.classList.add('active');
+  });
 }
